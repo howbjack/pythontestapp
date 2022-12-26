01404096dc3e5751723b2a61b2d0c9d9525d4f8a
Howard Jackson <howardbjackson@yahoo.com>
2022-12-26 13:03:46 -0500

    First Commit

---
+++ FirstPythonProgram.py
@@ -0,0 +1,8 @@
+import math
+def calc_area (radius):
+    area = math.pi * radius * radius
+    return area
+
+r = int(input("Enter the Radius: "))
+the_area=calc_area(r)
+print(f"The area is {the_area}")
