2b79a017a1598b7aa95cc808127e71dd39c1ca92
Howard Jackson <howardbjackson@yahoo.com>
2022-12-27 10:29:28 -0500

    Revert "Added Circumfrence calcuator - no function used"
    This reverts commit 3f2e083e442e7b57f450c04cb285ff0ee3ef8ff8.

--- FirstPythonProgram.py
+++ FirstPythonProgram.py
@@ -5,6 +5,4 @@ def calc_area (radius):
 
 r = int(input("Enter the Radius: "))
 the_area=calc_area(r)
-print(f"The area of the circle is {round(the_area,2)}") 
-the_circum= 2*math.pi*r
-print(f"The circumfrence is: {round(the_circum,2)} ")
+print(f"The area of the circle is {the_area}") 
