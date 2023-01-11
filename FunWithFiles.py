468354fc506a657503960bee2e568bd429fae10c
Howard Jackson <howardbjackson@yahoo.com>
2023-01-11 15:53:57 -0500

    First change for 2023

--- FunWithFiles.py
+++ FunWithFiles.py
@@ -5,9 +5,9 @@ with open('C:\downloads\File2.txt') as f:
     file2 = f.readlines()
 
 print(f"Contents of file1, length: {len(file1)}")
-#print(file1)
+print(file1)
 print(f"Contents of file2, length: {len(file2)}")
-#print(file2)
+print(file2)
 if (len(file1) < len(file2)):
     combo_len = len(file1)
     #single_len = len(file2) - len(file1)
@@ -22,20 +22,26 @@ f = open('C:\downloads\FileCombo.txt','w+')
 for i in range(combo_len):
     combo_line = file1[i].strip() + " " + file2[i]
     #print(i)
-    #print(combo_line)
+    print(combo_line)
     f.write(combo_line)
-f.write('\n')
+#f.write('\n')
 
 if (file_longer == "file1"):
+    f.write('\n')
     #print(combo_len, len(file1))
     for i in range(combo_len,len(file1)):
-        #print(file1[i])
+        print(file1[i])
         f.write(file1[i])
 else:
     #print(combo_len, len(file2))
     for i in range(combo_len,len(file2)):
-        #print(file2[i])
+        print(file2[i])
         f.write(file2[i])
 
 f.close()
-print("Combined file FileCombo.txt written")
+word_count = 0 
+with open('C:\downloads\FileCombo.txt') as f:
+    line = f.read()
+    print(line)
+    word_count += len(line.split(" "))
+print(f"Combined file FileCombo.txt written, total number of words is: {word_count}")
