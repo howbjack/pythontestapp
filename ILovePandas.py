b264658cc0e2208ed118401497ab5cce50426b1e
Howard Jackson <howardbjackson@yahoo.com>
2023-02-12 09:05:54 -0500

    Add logic to transpose rows and columns

--- ILovePandas.py
+++ ILovePandas.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-cs_df = pd.read_csv('C:\downloads\MyStmt.csv')
-print(cs_df) 
-print(cs_df['Amount'].mean())
+
+cs1_df = pd.read_csv('C:\downloads\MyCSVFile1.csv')
+print(cs1_df) 
+cs2_df = cs1_df.T # Transpose dataframe
+print(cs2_df)
+
+cs2_df.to_csv('C:\downloads\MyCSVFile2.csv',header=False)  # Remove header line, write to new file
+#print(cs_df['Amount'].mean())
 #print (cs_df.describe)
