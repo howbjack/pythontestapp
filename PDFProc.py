d862191b298c35540d0a72c27e06bbcc6281faf6
Howard Jackson <howardbjackson@yahoo.com>
2023-02-18 17:36:43 -0500

    Updated file path

--- PDFProc.py
+++ PDFProc.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import sys
 
-inputs = sys.argv[1:]
+
 
 
 def pdfcombine(pdf_list):
@@ -12,10 +12,10 @@ def pdfcombine(pdf_list):
         merger.append(pdf)
     merger.write('output.pdf')
 
-def pdfsplit(pdf):
+def pdfsplit():
 
-    sample_pdf1 = open('VenueSigned1.pdf', mode='rb')
-    sample_pdf2 = open('VenueSigned2.pdf', mode='rb')
+    sample_pdf1 = open('C:\downloads\VenueSigned1.pdf', mode='rb')
+    sample_pdf2 = open('C:\downloads\VenueSigned2.pdf', mode='rb')
 
     input_pdf1 = PyPDF2.PdfReader(sample_pdf1)
     input_pdf2 = PyPDF2.PdfReader(sample_pdf2)
@@ -24,7 +24,7 @@ def pdfsplit(pdf):
     output_pdf.add_page(input_pdf1.pages[0])
     output_pdf.add_page(input_pdf1.pages[1])
     output_pdf.add_page(input_pdf1.pages[2])
-    output_pdf.add_page(input_pdf2.pages[0])
+    output_pdf.add_page(input_pdf2.pages[0])   #  Insert page from second document 
     output_pdf.add_page(input_pdf1.pages[4])
     output_pdf.add_page(input_pdf1.pages[5])
     output_pdf.add_page(input_pdf1.pages[6])
@@ -32,11 +32,11 @@ def pdfsplit(pdf):
     output_pdf.add_page(input_pdf1.pages[8])
 
 
-    with open("output.pdf","wb") as output_file: 
+    with open("C:\downloads\output2.pdf","wb") as output_file: 
         output_pdf.write(output_file)
 
 
-
+inputs = sys.argv[1:]
 ## pdfcombine(inputs)
-pdfsplit('VenueSigned1.pdf')
+pdfsplit()
 
