b776f9411cb467938def135bacd885ca7293fdc3
Howard Jackson <howardbjackson@yahoo.com>
2023-04-21 19:42:35 -0400

    Clean Code Revision #1

--- hang_man.py
+++ hang_man.py
@@ -1,23 +1,26 @@
+"""My attempt to build a hangman game"""
 import random
 
 wordpool = ["welcome","fantasy","noodles","aadvark","unlikely","mississippi"]
 
 # This is a comment
 # Added another HBJ
-# Third line 
+# Third line
 
 def selectword():
-
+    """Uses a random number generator to choose the word for the game"""
     return wordpool[random.randint(0,5)]
 
-def getemptylist(n):
+def getemptylist(strlen):
+    """Creates the list that will be filled by the guessed letters"""
 
     my_list = []
-    for _ in range(n):
+    for _ in range(strlen):
         my_list.append('')
     return my_list
-   
+
 def checkposition(letter):
+    """Finds the position of the guessed letter, if it exists"""
     # Check the position of the guessed letter in word
     for i in range(len(gameletters)):
         if letter == gameletters[i]:
