6f9a76e3712e1c4d6f3873885e5723504ae75e09
Howard Jackson <howardbjackson@yahoo.com>
2023-04-21 12:50:45 -0400

    Added test comment

---
+++ Hangman.py
@@ -0,0 +1,32 @@
+import random
+
+wordpool = ["welcome","fantasy","noodles","aadvark","unlikely","mississippi"]
+
+def selectword():
+
+    return wordpool[random.randint(0,5)]
+
+def getemptylist(n):
+
+    my_list = []
+    for _ in range(n):
+        my_list.append('')
+    return my_list
+   
+def checkposition(letter):
+    # Check the position of the guessed letter in word
+    for i in range(len(gameletters)):
+        if letter == gameletters[i]:
+            guessletters[i] = letter
+
+
+gameletters = list(selectword())
+print (gameletters)
+
+guessletters = getemptylist(len(gameletters))
+print(guessletters)
+
+while gameletters != guessletters:
+    guess = input("Guess a letter: ")
+    checkposition(guess)
+    print(guessletters)
