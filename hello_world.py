5aadb504f1bff22d1601715050f727f9484e820d
Howard B Jackson <howardbjackson@gmail.com>
2023-04-22 11:50:34 -0500

    Changed Hello Message

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 """Sample Hello Program"""
 
 MyName = input("Enter your name: ")
-print (f"Hello to the Flipping Entire World, {MyName}")
+print (f"Hello  World, {MyName}")
 print ("I am Done")
 print("Finally we have a change. \n")
 print("One more Line")
